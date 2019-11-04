23287dd1e27daa0b0e48d120b1d769d619e5157f
unknown <dekunbifola100@gmail.com>
2019-11-04 08:55:51 +0100

    Fixes

--- src/index.js
+++ src/index.js
@@ -12,6 +12,18 @@ import * as serviceWorker from './serviceWorker';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  alert('Add to homescreen to get better experience')
+}
+
 const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
 
 const jsx = (
