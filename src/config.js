68cec7eca10563dff13dde3fd449195271d0fbd9
Kyle Kitlinski <kylekitlinski@gmail.com>
2019-11-08 13:40:23 -0500

    working

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // const api = "https://hearbk-server.herokuapp.com";
-const api = "http://localhost:3009";
-// const api = "https://hearbk.appspot.com";
+// const api = "http://localhost:3009";
+const api = "https://hearbk.appspot.com";
 export default api;
