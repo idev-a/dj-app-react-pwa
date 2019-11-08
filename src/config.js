ef368a547009d37546fce65c1a05c4fa56a819a5
Kyle Kitlinski <kylekitlinski@gmail.com>
2019-11-08 00:03:27 -0500

    temp patch api

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const api = "https://hearbk-server.herokuapp.com";
+// const api = "https://hearbk-server.herokuapp.com";
 // const api = "http://localhost:3009";
+const api = "https://hearbk.appspot.com";
 export default api;
