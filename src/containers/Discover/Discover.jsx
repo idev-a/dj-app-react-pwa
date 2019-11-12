26569de491415a873e5adf5e3263c0b301408dcb
nazmul <nazmul@maptaskr.com>
2019-11-12 23:38:03 +0600

    Player UI done

--- src/containers/Discover/Discover.jsx
+++ src/containers/Discover/Discover.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Cards, { Card } from 'react-swipe-card';
+import ReactWaves from '@dschoon/react-waves';
+import africa from '../../assets/sound/track.mp3';
 import PropTypes, { number } from "prop-types";
 import { connect } from "react-redux";
 import "./Discover.styles.scss";
@@ -13,6 +15,9 @@ import profilePicture from "../../assets/img/discover/oh4js6qs.png"
 import start1 from "../../assets/img/discover/dolar.png"
 import start2 from "../../assets/img/discover/star.png"
 import start3 from "../../assets/img/discover/graph.png"
+import start from "../../assets/img/discover/play.png"
+import next from "../../assets/img/discover/next.png"
+import previous from "../../assets/img/discover/previous.png"
 import {BottomMenu} from "../../components/bottomMenu"
 import {GroupButton} from "../../components/groupButton"
 
@@ -50,24 +55,27 @@ var data = [{
   }];
 
 const SwipeWrapper = () => {
-
+  const [state, setState] = useState({ ...data });
   function changeShowDiv(number, position){
     if(number< data.length-1){
       data[number].show = false
       data[number+1].show = true
     }
-    console.log(position, data);
+  }
+  function playPause(id){
+    data[id].show = data[id].show ? false : true;
+    console.log(this.state);
+    
   }
   return (
     <Cards
-    alertRight={<h1 className="alert-right-text">COOL</h1>}
-    alertTop={<h1 className="alert-right-text">HITS</h1>}
+    alertRight={<h1 className="alert-right-text">HIT</h1>}
+    alertTop={<h1 className="alert-right-text">COOL</h1>}
     alertLeft={<h1 className="alert-right-text">MISS</h1>}
     onEnd={() => console.log("Ënd")
     } className='master-root'>
         {data.map((item, index) => 
           <Card
-            
             onSwipeTop={() => changeShowDiv(index, "Top")}
             onSwipeLeft={() => changeShowDiv(index, "Left")} 
             onSwipeRight={() => changeShowDiv(index, "Right")}>
@@ -78,11 +86,38 @@ const SwipeWrapper = () => {
                   </div>
                   <TextSection text={item.name} paddingTop="33px" paddingBottom="2px" size="20px" color="#1B3543" weight="bold"/>
                   <TextSection text={item.fullName} color="#1B3543" paddingTop="0px" paddingBottom="20px" size="12px"/>
-                  <div className={'playerSection'}>
-                  <audio className="audio-element">
-                    <source src="https://api.coderrocketfuel.com/assets/pomodoro-times-up.mp3"></source>
-                  </audio>
-                    <img src={equlizer}/>
+                  <ReactWaves
+                    audioFile={africa}
+                    className='react-waves'
+                    options={{
+                      barGap: 2,
+                      barWidth: 2,
+                      barHeight: 2,
+                      cursorWidth: 1,
+                      height: 80,
+                      width: 350,
+                      hideScrollbar: true,
+                      progressColor: '#3788B2',
+                      normalize: true,
+                      responsive: true,
+                      waveColor: '#D8D8D8',
+                    }}
+                    volume={1}
+                    zoom={1}
+                    playing={item.show}
+                    pos={0}
+                    onPosChange={()=>console.log("chan")}
+                  />
+                  <div className={'playerControlSection'}>
+                    <div className={'playerControl'}>
+                      <img src={previous}/>
+                    </div>
+                    <div className={'playing'} onClick={()=>playPause(index)}>
+                      <img src={start}/>
+                    </div>
+                    <div className={'playerControl'}>
+                      <img src={next}/>
+                    </div>
                   </div>
                 </RoundCard>
           </Card>
@@ -97,8 +132,6 @@ const defaultValues = {
 
 const Discover = props => {
   const [state, setState] = useState({ ...defaultValues });
-  const data = ['Alexandre', 'Thomas', 'Lucien']
-
   return (
     <div className="bg-colored">
       <section className="section-adjust flex-center">
@@ -113,7 +146,7 @@ const Discover = props => {
                 <GroupButton />
                 </div>
               </div>
-              <TextSection text="Discover & Rate New Tracks" paddingTop="20px" paddingBottom="5px" size="15px"/>
+              <TextSection text="Discover & Rate New Tracks" paddingTop="30px" paddingBottom="5px" size="15px"/>
               <SwipeWrapper/>
               <RoundCard top="20px">
                   <TextSection text="My Activity" paddingTop="0px" paddingBottom="2px" size="18px" color="#1B3543" weight="bold"/>
