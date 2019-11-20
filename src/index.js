3ab1ef860869d148f7fcd11734288e567b0d0aa9
Pranay Tripathi <pranaytripathi123@gmail.com>
2019-11-20 23:02:21 +0100

    Made changes for IoS homescreen (#5)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "./styles/styles.scss";
@@ -9,30 +9,15 @@ import { Provider } from "react-redux";
 import reducers from "./reducers/index";
 import thunk from "redux-thunk";
 import * as serviceWorker from "./serviceWorker";
-import AddToHomeScreen from "./components/AddToHomeScreen";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const isIos = () => {
-  const userAgent = window.navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test(userAgent);
-};
-// Detects if device is in standalone mode
-const isInStandaloneMode = () =>
-  "standalone" in window.navigator && window.navigator.standalone;
-
-// Checks if should display install popup notification:
-const showIOSNotification = isIos() && !isInStandaloneMode();
 const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
 const App = () => {
-  const [showAddToScreenIOSOverlay, setShowAddToScreenIOSOverlay] = useState(showIOSNotification);
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <div className="mainWrapper">
-          <AppRouter />
-          {showAddToScreenIOSOverlay && <AddToHomeScreen closeClick={() => setShowAddToScreenIOSOverlay(false)}/>}
-        </div>
+        <AppRouter />
       </Provider>
     </BrowserRouter>
   );
