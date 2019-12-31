5d13a4109f5c306c4eaf3a7ddff5bb2eb1eb91ad
Eugene Kim <ekim1707@gmail.com>
2019-12-31 16:26:39 -0500

    first draft of popup component

---
+++ src/components/PopUps/PopUpsComponent.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import cx from "classnames";
+import content from "./content";
+import "./PopUps.styles.scss";
+import Icon from "../../common/IconComponent";
+import InputField from "../../common/InputField";
+import Button from "../../common/Button";
+
+const PopUpsComponent = ({
+    nameOnCard,
+    cardNumber,
+    expirationDate,
+    cvc,
+    onInputChange
+}) => {
+    return (
+        <div className="popUpContainer">
+            <div className="iconContainer">
+                <div className="headerIconContainer">
+                    <Icon className={cx("headerIcon")} iconName={"icon_white"} />
+                </div>
+                <Icon className={cx("cancelIcon")} iconName={"cancel"} />
+            </div>
+            <div className="popUpHeaderContainer">
+                {content.MAIN_HEADER}
+            </div>
+            <div className="inputFieldContainer">
+                <div className="inputFieldHeader">
+                    {content.INPUT_HEADER_1}
+                </div>
+                <InputField
+                    id="nameOnCard"
+                    onChange={onInputChange}
+                    value={nameOnCard}
+                    placeholder={content.INPUT_PLACEHOLDER_1}
+                    className="inputField"
+                />
+                <div className="inputFieldHeader">
+                    {content.INPUT_HEADER_2}
+                </div>
+                <InputField
+                    id="cardNumber"
+                    onChange={onInputChange}
+                    value={cardNumber}
+                    placeholder={content.INPUT_PLACEHOLDER_2}
+                    className="inputField"
+                />
+                <div className="inputFieldRow">
+                    <div className="inputFieldCols colLeft">
+                        <div className="inputFieldHeader">
+                            {content.INPUT_HEADER_3}
+                        </div>
+                        <InputField
+                            id="expirationDate"
+                            onChange={onInputChange}
+                            value={expirationDate}
+                            placeholder={content.INPUT_PLACEHOLDER_3}
+                            className="inputField"
+                        />
+                    </div>
+                    <div className="inputFieldCols">
+                        <div className="inputFieldHeader">
+                            {content.INPUT_HEADER_4}
+                        </div>
+                        <InputField
+                            id="cvc"
+                            onChange={onInputChange}
+                            value={cvc}
+                            placeholder={content.INPUT_PLACEHOLDER_4}
+                            className="inputField"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="buttonWrapper">
+                <Button
+                className="launchButton"
+                buttonText={content.SAVE_CARD_LABEL}
+                onClick={""}
+                ></Button>
+            </div>
+        </div>
+    );
+};
+
+PopUpsComponent.defaultProps = {
+    nameOnCard: "",
+    cardNumber: "",
+    expirationDate: "",
+    cvc: ""
+}
+
+export default PopUpsComponent;
