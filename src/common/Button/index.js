0a867f3ce6457991aeaf9624b0eb9c17dc856c35
Eugene Kim <ekim1707@gmail.com>
2020-01-12 11:39:15 -0500

    attempted merge

--- src/common/Button/index.js
+++ src/common/Button/index.js
@@ -2,21 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import IconComponent from "../IconComponent";
 
-<<<<<<< HEAD
-const Button = ({ className, iconClassName, onClick, buttonText, hasIcon, isIcon, iconName, disabled }) => {
-  return !isIcon ? (
-    !hasIcon ? 
-    <button className={className} onClick={onClick} disabled={disabled}>
-      {buttonText}
-    </button> :
-    <button className={className} onClick={onClick} disabled={disabled}>
-      {buttonText}
-=======
 const Button = ({ className, iconClassName, onClick, buttonText, isIcon, iconName, disabled }) => {
   return !isIcon ? (
     <button className={className} onClick={onClick} disabled={disabled}>
       {buttonText}
->>>>>>> changed Button icon incorporation method
       <IconComponent className={iconClassName} iconName={iconName} />
     </button>
   ) : (
--- src/components/ListenerPreferences/ListenerPreferencesComponent.js
+++ src/components/ListenerPreferences/ListenerPreferencesComponent.js
@@ -56,7 +56,6 @@ const ListenerPreferencesComponent = ({
         };
         containerContents = 
         <React.Fragment>
-<<<<<<< HEAD
             <DetailsForm 
                 handleClickToggleAddList={handleClickToggleAddList}
                 handleClickAddGenres={handleClickAddGenres}
@@ -67,17 +66,6 @@ const ListenerPreferencesComponent = ({
                 tagsAdded={tagsAdded}
             />
             <PriceForm />
-=======
-            <div className="detailsContainer">
-                <div className="detailsHeader">
-                    {content.DETAILS_TITLE}
-                </div>
-                <Details />
-            </div>
-            <div className="priceContainer">
-                <Price />
-            </div>
->>>>>>> minor adjustments
             <div className="buttonWrapper">
                 <Button
                 className="launchButton"
