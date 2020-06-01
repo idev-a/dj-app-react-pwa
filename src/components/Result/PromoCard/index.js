6c02843b9cbfedc661f3bd4fd271cd5754ea7948
Ali <akbar@softsuave.com>
2020-06-01 19:33:46 +0530

    Interated result screen with backend

--- src/components/Result/PromoCard/index.js
+++ src/components/Result/PromoCard/index.js
@@ -10,6 +10,7 @@ import { ReactComponent as Social } from '../../../assets/icon/social.svg';
 import { ReactComponent as ThumbsDown } from '../../../assets/icon/ThumbsDown.svg';
 import { ReactComponent as Edit } from '../../../assets/icon/Edit.svg';
 import { ReactComponent as Key } from '../../../assets/icon/access.svg';
+import moment from 'moment';
 
 export const IconContainer = ({ icon, number, text }) => (
     <div className="icon-container-2">
@@ -19,7 +20,7 @@ export const IconContainer = ({ icon, number, text }) => (
     </div>
 );
 
-const PromoCard = ({ hasKey, hasFeedback }) => {
+const PromoCard = ({ hasKey, hasFeedback, cardData }) => {
 
     const [cardIsOpen, setCardIsOpen] = useState(false);
 
@@ -27,16 +28,19 @@ const PromoCard = ({ hasKey, hasFeedback }) => {
         setCardIsOpen(!cardIsOpen)
     }, [cardIsOpen, setCardIsOpen]);
 
+    const hitPercentageRounded = Math.round(cardData.stats.hit);
+    const missPercentageRounded = Math.round(cardData.stats.miss);
+
     return (
         <div className={hasKey ? "promo-campaign-card-border" : "promo-campaign-card"}>
             <div className="promo-camaign-inner-container">
                 <div className="like-container">
                     <Like className="like-icon" />
-                    <span className="like-percentage-txt">90%</span>
+                    <span className="like-percentage-txt">{hitPercentageRounded ? hitPercentageRounded : 0}%</span>
                 </div>
                 <div className="promo-campaign-inner-container">
                     <div className="card-container-2">
-                        <small className="subscription-date-text">05/01/2020 - 05/06/2020</small>
+                        {cardData && <small className="subscription-date-text">{`${moment(cardData.created_on).format("MM/DD/YYYY")} - ${moment(cardData.updated_on).format("MM/DD/YYYY")}`}</small>}
                         {!cardIsOpen &&
                             <div className="card-container-3">
                                 <Like className="icon-2" />
@@ -57,14 +61,14 @@ const PromoCard = ({ hasKey, hasFeedback }) => {
             {cardIsOpen &&
                 <div className="card-inner-container">
                     <div className="title-track-container">
-                        <small>Title track</small>
+                        <small>{cardData.trackTitle}</small>
                         <small>4:11</small>
                     </div>
                     <div className="card-icons-container">
                         <div className="icon-first-row">
-                            <IconContainer icon={<Social className="icon-3" />} number="299" text="Listners" />
-                            <IconContainer icon={<Like className="icon-3" />} number="90%" text="Thumbs Up" />
-                            <IconContainer icon={<ThumbsDown className="icon-3" />} number="2%" text="Thumbs Down" />
+                            <IconContainer icon={<Social className="icon-3" />} number={cardData.listenersCount} text="Listners" />
+                            <IconContainer icon={<Like className="icon-3" />} number={hitPercentageRounded ? hitPercentageRounded + "%" : "0%"} text="Thumbs Up" />
+                            <IconContainer icon={<ThumbsDown className="icon-3" />} number={missPercentageRounded ? missPercentageRounded+ "%" : "0%"} text="Thumbs Down" />
                         </div>
                         <div className="icon-second-row">
                             <IconContainer icon={<Happy className="icon-3" />} number="299" text="Impressed" />
