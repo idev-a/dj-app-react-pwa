df4fc32199e67bc2cdbcc00c35ae1430cf58b8cd
Ali <akbar@softsuave.com>
2020-06-22 18:11:08 +0530

    Fixed leaderboard 1 header, footer and styling issues.

--- src/components/Leaderboard1/LeaderboardComponent.js
+++ src/components/Leaderboard1/LeaderboardComponent.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './LeaderboardComponent.styles.scss';
 import content from "./content";
-import { LeftArrowIcon, GoldCoinIcon } from './../../assets/icon/svgicon';
+import { LeftArrowIcon } from './../../assets/icon/svgicon';
+
+import { ReactComponent as MoneyBag } from '../../assets/icon/MoneyBag.svg';
+import { ReactComponent as FireIcon } from '../../assets/icon/FireIcon.svg';
 
 const LeaderboardComponent = ({ user }) => {
 
@@ -13,27 +16,15 @@ const LeaderboardComponent = ({ user }) => {
                     <div className="left-section">
                         <svg className="left-arrow-icon"><LeftArrowIcon /></svg>
                     </div>
-                    <div className="right-section">
-                        <div className="gold-coin">
-                            <svg className="gold-coin-icon"><GoldCoinIcon /></svg>
-                            <span className="coins-value">
-                                {content.COINS_VALUE}
-                            </span>
-                            <span className="coins-text">
-                                {content.COINS_TEXT}
-                            </span>
+                    <div className="header-icon-container">
+                        <MoneyBag className="header-icon" />
+                        <div className="header-icon-text-container">
+                            <p className="coin-number">{user.balance}</p><p className="coin-text">{content.COINS_TEXT}</p>
                         </div>
-                        <div className="rating">
-                            <div className="rating-circle">
-                                <div className="left-half-circle"></div>
-                                <span className="rating-value">{content.RATING_VALUE}</span>
-                                <div className="right-half-circle"></div>
-                            </div>
-                            <span className="rating-text">
-                                {content.RATING_TEXT}
-                            </span>
+                        <FireIcon className="header-icon" />
+                        <div className="header-icon-text-container" >
+                            <p className="coin-number">Lv.3</p><p className="coin-text">{content.RATING_TEXT}</p>
                         </div>
-                        
                     </div>
                 </div>
 
@@ -43,18 +34,18 @@ const LeaderboardComponent = ({ user }) => {
                         <div className="tracks">{content.TRACKS}</div>
                     </div>
                     {content.LISTENERS_DATA.map((list, index) => {
-                        return(
+                        return (
                             <div className="listeners-data-row">
                                 <div className="serial-number">
                                     <span className="serial-number-circle">
                                         <span className="serial-number-value">
-                                            {index+1}
+                                            {index + 1}
                                         </span>
                                     </span>
                                 </div>
                                 <div className="listener-name-image">
-                                    <div className="listener-image" 
-                                        style={{background: `url(${list.image}) 0% 0% / cover no-repeat`}}>
+                                    <div className="listener-image"
+                                        style={{ background: `url(${list.image}) 0% 0% / cover no-repeat` }}>
                                     </div>
                                     <p className="listener-name">{list.name}</p>
                                 </div>
