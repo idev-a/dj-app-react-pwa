b77941799864e1f2ad4eb8c51bf2a0fa3839e149
Ali <akbar@softsuave.com>
2020-06-23 17:25:37 +0530

    Implemented music creators screen.

--- src/components/DisoverMusic/DiscoverMusicContainer.js
+++ src/components/DisoverMusic/DiscoverMusicContainer.js
@@ -1,32 +1,40 @@
-import React, {useState, useCallback} from 'react';
+import React, { useState, useCallback } from 'react';
 import content from './content';
 import './DiscoverMusic.styles.scss';
 import cx from 'classnames';
+import history from "../../history";
 import { ReactComponent as Back } from '../../assets/icon/back.svg';
 import DiscoverNewMusic from './DiscoverNewMusic';
 import MusicInfluencer from './MusicInfluencer';
 import BeakerFame from './BeakerFame';
+import MusicCreators from './MusicCreators';
 
 const DiscoverMusicContainer = () => {
 
-    const[currentScreen, setCurrentScreen] = useState(0);
-    
+    const [currentScreen, setCurrentScreen] = useState(0);
+
     const handleOnClickBack = useCallback(() => {
-        if(currentScreen >= 0){
-            setCurrentScreen(currentScreen-1)
+        if (currentScreen > 0) {
+            setCurrentScreen(currentScreen - 1)
         }
     }, [currentScreen])
 
     const handleOnClickNext = useCallback(() => {
-        if(currentScreen < 4){
-            setCurrentScreen(currentScreen+1)
+        if (currentScreen < 3) {
+            setCurrentScreen(currentScreen + 1)
+        } else {
+            history.push('/splash')
         }
     }, [currentScreen])
+
+    const handleOnClickSkip = () => {
+        history.push('/splash')
+    }
 
     return (
         <div className="discover-music-container">
             <section className="discover-music-container-section-1">
-                <small className="discover-skip-text">{content.SKIP}</small>
+                <small className="discover-skip-text" onClick={handleOnClickSkip}>{content.SKIP}</small>
                 <div className="discover-logo-container">
                     <div className="discover-logo-content" />
                     <span className="discover-breaker-text">{content.BREAKER}</span>
@@ -35,6 +43,7 @@ const DiscoverMusicContainer = () => {
             {currentScreen === 0 && <DiscoverNewMusic />}
             {currentScreen === 1 && <MusicInfluencer />}
             {currentScreen === 2 && <BeakerFame />}
+            {currentScreen === 3 && <MusicCreators />}
             <section className="discover-music-footer-container">
                 <Back className="discover-back-icon" onClick={handleOnClickBack} />
                 <div className="progress-dots-container">
